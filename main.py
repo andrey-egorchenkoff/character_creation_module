a9e390dcd87d4c4fb75b21f1aeb930044d973955
unknown <eas85@LAPTOP>
2022-10-23 22:00:41 +0300

    character_creation_module

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import randint
 
 
-
 from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -93,7 +93,3 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-
-   
