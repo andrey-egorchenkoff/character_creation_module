e9a0ac28b29e90f36bb363c0e481e3ce564dc652
unknown <eas85@LAPTOP>
2022-10-27 21:13:26 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -94,6 +94,6 @@ if __name__ == '__main__':
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
+    print('Воитель, Магг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
